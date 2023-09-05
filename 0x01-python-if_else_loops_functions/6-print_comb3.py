a75c0c6954c31ff46748ea7715917a8c94602b9b
Chamour15 <wafae.chamour@gmail.com>
2023-09-05 15:39:22 -0700

    6-print_comb3.py

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+for digits in range(0, 10):
+    for _digits in range(digits + 1, 10):
+        if digits == 8 and _digits == 9:
+            print("{}{}".format(digits, _digits))
+        else:
+            print("{}{}".format(digits, _digits), end=", ")
